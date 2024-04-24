9631b768c290223071c4e79ceadd3c6b73921e8c
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-04-23 21:16:37 -0300

    DDL
    Criação do DATABASE livraria e geração do modelo ERR.
    Tabelas criadas:
    - autor;
    - editora;
    - livro.
    Ademais, comandos de criação foram utilizados. Alguns deles são: CREATE, DROP, PRIMARY KEY, FOREIGN KEY, INDEX etc.

---
+++ MySQL-123H-02/MySQL-123H-02-DDL-Livraria.sql
@@ -0,0 +1,43 @@
+CREATE DATABaSE livraria
+DEFAULT CHARACTER SET utf8mb4
+DEFAULT COLLATE utf8mb4_0900_as_cs;
+
+USE livraria;
+
+CREATE TABLE autor (
+	id_autor INT AUTO_INCREMENT PRIMARY KEY,
+    nome_autor VARCHAR(45),
+    sobrenome VARCHAR(40)
+);
+
+CREATE TABLE editora (
+	id_editora INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- como é PRIMARY KEY, torna-se desnecessário colocar como NOT NULL
+	nome_editora VARCHAR(45) NULL -- não é necessário colocar NULL, pois automaticamente é setado como NULL se não for NOT NULL
+);
+
+-- DROP DATABASE livraria; 
+-- DROP TABLE editora; 
+
+CREATE TABLE livro (
+	id_livro INT AUTO_INCREMENT PRIMARY KEY,
+	nome_livro VARCHAR(45) NOT NULL,
+    id_autor INT NOT NULL, -- posso ter FOREIGN KEY recebendo valores NULL
+    id_editora INT NOT NULL,
+    
+	INDEX fk_id_autor (id_autor), -- criando indíces p/ melhor performance
+    INDEX fk_id_editora (id_editora), -- criando indíces p/ melhor performance
+    
+    CONSTRAINT fk_id_autor FOREIGN KEY (id_autor) 
+		REFERENCES autor(id_autor)
+	ON UPDATE RESTRICT 
+    ON DELETE RESTRICT, -- padrão, mesmo se não mencionar (Integridade referencial de proteção contra UPDADE e DELETE da FOREIGN KEY)
+
+	CONSTRAINT fk_id_editora FOREIGN KEY (id_editora)
+		REFERENCES editora(id_editora)
+	ON UPDATE RESTRICT 
+    ON DELETE RESTRICT
+);
+
+-- DROP TABLE livro;
+-- DROP TABLE autor;
+-- DROP TABLE editora;
